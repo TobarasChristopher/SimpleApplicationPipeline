dd21cb6ca22a8485d88fdf0e44f57c9bc1440e1a
unknown <tobaraschristopher@yahoo.ie>
2024-03-12 17:32:00 +0000

    add continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop simple_application
+
+cd SimpleApplicationPipeline/
+
+npm install
+
+pm2 start ./bin/www --name simple_application
