6c77b3f49df97392e466445c06e20adc21601a8a
unknown <tobaraschristopher@yahoo.ie>
2024-03-14 15:37:29 +0000

    reversed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,8 @@
 sudo apt update && sudo apt install nodejs npm
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-nvm install 20.5.0
-nvm use 20.5.0
-
-node -v
-npm -v
-
 sudo npm install -g pm2
 
 pm2 stop SimpleApplicationPipeline
-
-
 
 cd SimpleApplicationPipeline/
 
