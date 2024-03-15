50e861ee905242a0b72e08ca3a695c21eaf56d6a
unknown <tobaraschristopher@yahoo.ie>
2024-03-15 10:23:49 +0000

    update deploy var with nvm

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
-sudo apt update && sudo apt install nodejs npm
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y curl
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+
 nvm install 18
 nvm use 18
+
+echo "Node version:"
+node -v
+echo "npm version:"
+npm -v
 
 sudo npm install -g pm2
 
@@ -12,11 +23,7 @@ pm2 stop SimpleApplicationPipeline
 
 cd SimpleApplicationPipeline/
 
-npm -v
-node -v
-
 npm install
-
 
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
